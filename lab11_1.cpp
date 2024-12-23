05fc8040468bad13481581d9fe72ea84c151ef54
NNLLP <98796613+NNLLP@users.noreply.github.com>
2024-12-23 17:17:59 +0700

    lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        cin.ignore();
+    }
+
+    srand(time(0));
+
+    vector<string> grades = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    string predictedGrade = grades[rand() % grades.size()];
+
+    cout << "\nYou will get " << predictedGrade << " in this 261102." << endl;
+
+    return 0;
+}
